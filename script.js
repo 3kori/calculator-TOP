e55e56431216ee68eeae4ae6a72198c45fe04329
3kori <127242906+3kori@users.noreply.github.com>
2025-11-14 18:43:44 +0300

    Streamline the calculator functionality

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const operatorBtns = document.querySelectorAll(".operator-btns");
 const equalsBtn = document.querySelector( ".equals" );
 const decimalBtn = document.querySelector( ".decimal");
 const clearBtn = document.querySelector( ".clear" );
+const deleteBtn = document.querySelector( ".delete" );
 
 function add( x, y ) {
     return parseFloat(( x + y ).toFixed(10) );
@@ -31,6 +32,7 @@ let firstTerm;
 let secondTerm;
 let arithmeticOperator;
 let result;
+let calculationDone = false;
 
 function operate( operator, leftNo, rightNo ) {
     switch ( operator ) {
@@ -47,20 +49,29 @@ function operate( operator, leftNo, rightNo ) {
 
 numberBtns.forEach(( numberBtn ) => {
     numberBtn.addEventListener( "click", function ( e ) {
-        if (input.value === "Error") {
+        if ( calculationDone === true && arithmeticOperator === undefined ) {
+            input.value = "";
+            input.value += e.target.innerText;
+            firstTerm = parseFloat( input.value );
+            secondTerm = undefined;
+            
+            calculationDone = false;
+            console.log(firstTerm);
+            return;
+        }
+
+        if (calculationDone === true && arithmeticOperator !== undefined) {
+
+            input.value = e.target.innerText;
+            secondTerm = parseFloat(input.value);
+            calculationDone = false;
+            return;
+        }
+        else if (input.value === "Error") {
             numberBtns.forEach(( numberBtn ) => {
                 numberBtn.disabled = true;
                 console.log(numberBtn.disabled);
             });
-        }
-        else if ( result !== undefined ) {
-            input.value = "";
-            input.value += e.target.innerText;
-            result = undefined;
-            secondTerm = undefined;
-            arithmeticOperator = undefined;
-            firstTerm = parseFloat( input.value);
-            console.log(firstTerm);
         }
         else if ( arithmeticOperator !== undefined && secondTerm !== undefined ) {
             input.value += e.target.innerText;
@@ -95,11 +106,13 @@ operatorBtns.forEach(( operatorBtn ) => {
         else if ( firstTerm !== undefined && secondTerm !== undefined && arithmeticOperator !== undefined) {
             input.value = operate( arithmeticOperator, firstTerm, secondTerm );
             secondTerm = undefined;
-            firstTerm = parseFloat( input.value );
+            result = parseFloat( input.value );
             arithmeticOperator = operatorBtn.value;
-            console.log(firstTerm);
-            console.log(secondTerm);
-            console.log(arithmeticOperator);
+            firstTerm = parseFloat( result );
+            console.log("This is the firstterm when pressing the operator:", firstTerm);
+            console.log("This is the arithmetic operator when pressing the operator:",arithmeticOperator);
+            console.log("This is the secondterm when pressing the operator:",secondTerm);
+            console.log("This is the result when pressing the operator:", result);
         }
         else {
             arithmeticOperator = operatorBtn.value;
@@ -113,6 +126,9 @@ operatorBtns.forEach(( operatorBtn ) => {
 equalsBtn.addEventListener( "click", function() {
     if ( input.value === "Error" ) {
         equalsBtn.disabled = true;
+        firstTerm = undefined;
+        secondTerm = undefined;
+        arithmeticOperator = undefined;
         console.log( equalsBtn.disabled );
     }
     else if ( firstTerm === undefined ) {
@@ -126,29 +142,32 @@ equalsBtn.addEventListener( "click", function() {
     }
     else {
         input.value = operate( arithmeticOperator, firstTerm, secondTerm );
-        result = input.value;
-        if ( input.value === "Error" ) {
-            firstTerm = undefined;
-            secondTerm = undefined;
-            arithmeticOperator = undefined;
-            return;
-        }
-        console.log(firstTerm);
-        console.log(arithmeticOperator);
-        console.log(secondTerm);
+        result = parseFloat( input.value );
+        firstTerm = parseFloat( result );
+        result = undefined;
+        secondTerm = undefined;
+        arithmeticOperator = undefined;
+        decimalBtn.disabled = false;
+        calculationDone = true;
+        console.log("This is the firstterm when clicking equals sign:", firstTerm);
+        console.log("This is the arithmetic operator when clicking equals sign:", arithmeticOperator);
+        console.log("This is the secondterm when clicking equals sign:", secondTerm);
         console.log( "This the answer when clicking equals sign:", result);
     }
 });
 
 decimalBtn.addEventListener( "click", function() {
-    if ( firstTerm !== undefined && secondTerm === undefined && arithmeticOperator !== undefined ) {
-        input.value = ""
-        input.value = 0 + decimalBtn.value;
-        secondTerm = input.value;
+    if ( input.value === "Error" ) {
+        decimalBtn.disabled = true;
+    }
+    else if ( firstTerm !== undefined && secondTerm === undefined && arithmeticOperator !== undefined ) {
+        input.value = "0.";
+        secondTerm = parseFloat( input.value );
+        decimalBtn.disabled = true;
         console.log(secondTerm);
     }
     else if ( input.value === "" ) {
-        input.value = 0 + decimalBtn.value;
+        input.value = "0.";
         firstTerm = parseFloat( input.value );
         console.log(firstTerm);
     }
@@ -167,6 +186,7 @@ clearBtn.addEventListener( "click", function() {
     firstTerm = undefined;
     secondTerm = undefined;
     arithmeticOperator = undefined;
+    result = undefined;
     numberBtns.forEach(( numberBtn ) => {
         numberBtn.disabled = false;
         console.log(numberBtn.disabled);
@@ -181,4 +201,29 @@ clearBtn.addEventListener( "click", function() {
     console.log(firstTerm);
     console.log(secondTerm);
     console.log(arithmeticOperator);
+});
+
+deleteBtn.addEventListener( "click", function() {
+    if ( result !== undefined ) {
+        input.value = "";
+        firstTerm = undefined;
+        secondTerm = undefined;
+        arithmeticOperator = undefined;
+        result = undefined;
+        console.log("The result while deleting:", result);
+        return result;
+    }
+    else if ( secondTerm !== undefined && result === undefined ) {
+        input.value = input.value.toString().substring(0, input.value.length - 1);
+        secondTerm = parseFloat( input.value);
+        console.log("The secondterm while deleting:", secondTerm);
+        return secondTerm;
+    }
+    else if ( firstTerm !== undefined && arithmeticOperator === undefined && secondTerm === undefined ) {
+        input.value = input.value.toString().substring(0, input.value.length - 1);
+        firstTerm = parseFloat( input.value);
+        console.log("The firstterm while deleting:", firstTerm);
+        return firstTerm;
+    }
+
 });
